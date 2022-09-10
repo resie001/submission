84edd1efe84a471fe8032a41c7f6142a9ea49b35
ade.resie <ade.resie@privyid.tech>
2022-09-10 12:11:02 +0700

    Initial Commit

---
+++ submission/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  submission
+//
+//  Created by Ade Resie on 10/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
